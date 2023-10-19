62ad750ac723bee4f76eb29bab081e719c9383df
Elielton Ramos <elieltonramos14@gmail.com>
2023-10-19 11:40:24 -0300

    Criando testes para a camada services

--- backend/tests/unit/services/productsService.test.js
+++ backend/tests/unit/services/productsService.test.js
@@ -23,4 +23,20 @@ describe('Realizando testes unitários para a camada service', function () {
     expect(data).to.deep.equal({ message: 'Not found' });
     expect(status).to.be.equal('NOT_FOUND');
   });
+
+  it('listProductId deve retornar um produto e o status da requisição em caso de sucesso', async function () {
+    sinon.stub(models, 'findProductById').resolves([mockDBProducts[0]]);
+
+    const { status, data } = await services.listProductId(1);
+    expect(data).to.be.an('object');
+    expect(status).to.be.equal('OK');
+  });
+
+  it('listProductId deve retornar um message e o status da requisição em caso de falha', async function () {
+    sinon.stub(models, 'findProductById').resolves([]);
+
+    const { status, data } = await services.listProductId(999);
+    expect(data).to.deep.equal({ message: 'Product not found' });
+    expect(status).to.be.equal('NOT_FOUND');
+  });
 });

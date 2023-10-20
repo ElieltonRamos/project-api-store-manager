9397c867d0c6cf634db78191d1a3849fb4852602
Elielton Ramos <elieltonramos14@gmail.com>
2023-10-20 15:19:43 -0300

    Refatorando model de listagem de vendas

--- backend/src/models/sales.js
+++ backend/src/models/sales.js
@@ -3,22 +3,22 @@ const connection = require('./connection');
 const findAllSales = async () => {
   const [allSales] = await connection.execute(`
   SELECT
-  t1.sale_id,
+  t1.sale_id AS saleId,
   t3.date,
-  t2.id AS product_id,
+  t2.id AS productId,
   t1.quantity
 FROM sales_products AS t1
   INNER JOIN products AS t2 ON t1.product_id = t2.id
-  INNER JOIN sales AS t3 ON t1.sale_id = t3.id;`);
+  INNER JOIN sales AS t3 ON t1.sale_id = t3.id
+  ORDER BY t1.sale_id, t2.id;`);
   return allSales;
 };
 
 const findSalesById = async (id) => {
   const [sale] = await connection.execute(
     `SELECT
-  t1.sale_id,
   t3.date,
-  t2.id AS product_id,
+  t2.id AS productId,
   t1.quantity
 FROM sales_products AS t1
   INNER JOIN products AS t2 ON t1.product_id = t2.id

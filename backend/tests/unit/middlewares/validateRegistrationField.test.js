e81499dd88121d6bb023186715faa72cb915dfc5
Elielton Ramos <elieltonramos14@gmail.com>
2023-10-21 16:19:40 -0300

    Criando testes para od middares de verificacao

--- backend/tests/unit/middlewares/validateRegistrationField.test.js
+++ backend/tests/unit/middlewares/validateRegistrationField.test.js
@@ -7,15 +7,45 @@ chai.use(sinonChai);
 const middlewares = require('../../../src/middlewares');
 
 describe('Testes unitários - Middlewares - validateRegistrationField', function () {
-  it('validateRegistrationField', function () {
-    const next = sinon.stub();
-    const req = sinon.stub().returnsThis();
+  it('validateRegistrationField deve chamar o next em caso de sucesso', function () {
+    const next = sinon.stub().returns();
+    const req = { body: { name: 'Teste' } };
     const res = {
       status: sinon.stub().returnsThis(),
       json: sinon.stub().returnsThis(),
     };
 
     middlewares.validateRegistrationField(req, res, next);
-    // fazer teste desse middleware
+    expect(next).to.be.calledWith();
+  });
+
+  it('validateRegistrationField deve responder em caso de erro', function () {
+    const next = sinon.stub().returns();
+    const req = { body: {} };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub().returnsThis(),
+    };
+
+    middlewares.validateRegistrationField(req, res, next);
+    expect(next).to.be.not.calledWith();
+    expect(res.status).to.be.calledWith(400);
+    expect(res.json).to.be.calledWith({ message: '"name" is required' });
+  });
+
+  it('validateRegistrationField deve responder caso name seja menor que 5 letras', function () {
+    const next = sinon.stub().returns();
+    const req = { body: { name: 'Test' } };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub().returnsThis(),
+    };
+
+    middlewares.validateRegistrationField(req, res, next);
+    expect(next).to.be.not.calledWith();
+    expect(res.status).to.be.calledWith(422);
+    expect(res.json).to.be.calledWith({
+      message: '"name" length must be at least 5 characters long',
+    });
   });
 });
